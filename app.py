ab7366ccec239c7762726667dfde91af60483bfc
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-04-10 16:04:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
 import streamlit as st
-from rembg import remove
+import cv2
+import numpy as np
 from PIL import Image
 
-def process_image(input_image):
-    output_image = remove(input_image)
-    return output_image
+def remove_background(image):
+    # Converti l'immagine PIL in un array NumPy
+    img_array = np.array(image)
+
+    # Converti l'immagine da RGB a scala di grigi
+    gray = cv2.cvtColor(img_array, cv2.COLOR_RGB2GRAY)
+
+    # Applica una sogliatura per ottenere un'immagine binaria
+    _, thresh = cv2.threshold(gray, 240, 255, cv2.THRESH_BINARY)
+
+    # Trova i contorni nell'immagine binaria
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Crea una maschera per i contorni trovati
+    mask = np.zeros_like(gray)
+    cv2.drawContours(mask, contours, -1, (255, 255, 255), thickness=cv2.FILLED)
+
+    # Applica la maschera all'immagine originale per rimuovere lo sfondo
+    result = cv2.bitwise_and(img_array, img_array, mask=mask)
+
+    # Converti l'array NumPy risultante in un'immagine PIL
+    result_image = Image.fromarray(result)
+
+    return result_image
 
 # Streamlit app
 def main():
@@ -16,15 +38,13 @@ def main():
         input_image = Image.open(uploaded_file)
 
         # Process image
-        output_image = process_image(input_image)
+        output_image = remove_background(input_image)
 
         # Display processed image
         st.image(output_image, caption='Processed Image', use_column_width=True)
 
         # Button to download the processed image
-        output_path = 'removed.png'
-        output_image.save(output_path)
-        st.download_button(label='Download', data=open(output_path, 'rb').read(), file_name='output.png')
+        st.download_button(label='Download', data=output_image, file_name='output.png')
 
 if __name__ == "__main__":
     main()
